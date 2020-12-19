cc592e0fea9910332664d54290bc64682c30c6b2
Denis Pinheiro <denisppinheiro@gmail.com>
2020-12-19 16:18:44 -0300

    CodeMR analysis report upload.

---
+++ codemr/reprova/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"reprova", "Number of Highly Problematic Classes":0, "Number of Entities":37, "Number of Problematic Classes":0, "Class Lines of Code":1434, "Number of External Packages":18, "Number of Packages":8, "Number of External Entities":93}
---
+++ codemr/reprova/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,26 @@
+var matrix = [[0,0,0,0,0,0,0,0],[1,0,0,1,0,0,0,1],[0,0,0,1,0,1,1,0],[0,3,0,0,0,3,0,0],[0,1,1,1,0,1,0,0],[0,1,0,0,0,0,0,0],[0,3,0,3,0,3,0,0],[1,6,0,4,0,0,0,0]]
+var packages = [{
+"name": " br.ufmg.engsoft.reprova.model.difficulty", "color": " #3182bd"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.model", "color": " #6baed6"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.routes", "color": " #9ecae1"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.database", "color": " #c6dbef"
+}
+,{
+"name": " br.ufmg.engsoft.reprova", "color": " #e6550d"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.mime.json", "color": " #fd8d3c"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.routes.api", "color": " #fdae6b"
+}
+,{
+"name": " br.ufmg.engsoft.reprova.model.generator", "color": " #fdd0a2"
+}
+];
