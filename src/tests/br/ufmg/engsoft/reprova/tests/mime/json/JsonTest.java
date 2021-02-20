c99af7808bebec85fb4bd548ee6614d90b18e544
henriqueup <titepessoa@gmail.com>
2021-02-20 16:57:48 -0300

    Comenta teste desativado

--- src/tests/br/ufmg/engsoft/reprova/tests/mime/json/JsonTest.java
+++ src/tests/br/ufmg/engsoft/reprova/tests/mime/json/JsonTest.java
@@ -1,50 +1,50 @@
-package br.ufmg.engsoft.reprova.tests.mime.json;
+// package br.ufmg.engsoft.reprova.tests.mime.json;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
 
-import br.ufmg.engsoft.reprova.mime.json.Json;
-import br.ufmg.engsoft.reprova.model.Question;
+// import br.ufmg.engsoft.reprova.mime.json.Json;
+// import br.ufmg.engsoft.reprova.model.Question;
 
 
-public class JsonTest {
-  /**
-   * Rendering then parsing should produce an equivalent object.
-   */
-  @Test
-  @Disabled
-  void question() {
-    var question = new Question.Builder()
-      .id("id")
-      .theme("theme")
-      .description("description")
-      .statement("statement")
-      // .record(
-      //   Map.of(
-      //     new Semester(2019, Semester.Reference._1), Map.of(
-      //       "tw", 50.0f,
-      //       "tz", 49.5f,
-      //       "tx", 51.2f
-      //     ),
-      //     new Semester(2020, Semester.Reference._2), Collections.emptyMap()
-      //   )
-      // )
-      .pvt(false)
-      .estimatedTime(8)
-      .build();
+// public class JsonTest {
+//   /**
+//    * Rendering then parsing should produce an equivalent object.
+//    */
+//   @Test
+//   @Disabled
+//   void question() {
+//     var question = new Question.Builder()
+//       .id("id")
+//       .theme("theme")
+//       .description("description")
+//       .statement("statement")
+//       // .record(
+//       //   Map.of(
+//       //     new Semester(2019, Semester.Reference._1), Map.of(
+//       //       "tw", 50.0f,
+//       //       "tz", 49.5f,
+//       //       "tx", 51.2f
+//       //     ),
+//       //     new Semester(2020, Semester.Reference._2), Collections.emptyMap()
+//       //   )
+//       // )
+//       .pvt(false)
+//       .estimatedTime(8)
+//       .build();
 
-    var formatter = new Json();
+//     var formatter = new Json();
 
-    var json = formatter.render(question);
+//     var json = formatter.render(question);
 
-    var questionCopy = formatter
-      .parse(json, Question.Builder.class)
-      .build();
+//     var questionCopy = formatter
+//       .parse(json, Question.Builder.class)
+//       .build();
 
-    assertTrue(
-      question.equals(questionCopy)
-    );
-  }
-}
+//     assertTrue(
+//       question.equals(questionCopy)
+//     );
+//   }
+// }
