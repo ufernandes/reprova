a26de6c0ddf053f01c61dce2c5282d098d28396c
LorenaMendes <35077553+LorenaMendes@users.noreply.github.com>
2021-03-06 15:17:39 -0300

    Comments added

--- src/java/br/ufmg/engsoft/reprova/model/generator/GeneratorFactory.java
+++ src/java/br/ufmg/engsoft/reprova/model/generator/GeneratorFactory.java
@@ -1,7 +1,12 @@
 package br.ufmg.engsoft.reprova.model.generator;
 
+/*
+* The type GeneratorFactory
+*/
 public class GeneratorFactory{
-  
+  /*
+   * GeneratorFactory Constructor
+   */
   public IQuestionnaireGenerator getGenerator(int difficultiesCount) {
 	  if (difficultiesCount != 0){
 		  return new DifficultyGroupGenerator();
