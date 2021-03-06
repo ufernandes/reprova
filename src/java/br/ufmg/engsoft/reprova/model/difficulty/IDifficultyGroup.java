29d590d19bb405029fc40e156d7f49880402556d
LorenaMendes <35077553+LorenaMendes@users.noreply.github.com>
2021-03-06 15:37:05 -0300

    Comments added

--- src/java/br/ufmg/engsoft/reprova/model/difficulty/IDifficultyGroup.java
+++ src/java/br/ufmg/engsoft/reprova/model/difficulty/IDifficultyGroup.java
@@ -2,7 +2,11 @@ package br.ufmg.engsoft.reprova.model.difficulty;
 
 import java.util.List;
 
+/* The type IDifficultGroup */
 public interface IDifficultyGroup{
+  /* getDifficultyGroup */
   public int getDifficultyGroup(double avg);
+  
+  /* getDifficulties */
   public List<String> getDifficulties();
 }
