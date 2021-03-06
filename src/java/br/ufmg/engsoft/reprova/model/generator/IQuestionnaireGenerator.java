be943037229c45100cab942598aebb4abc4e748a
LorenaMendes <35077553+LorenaMendes@users.noreply.github.com>
2021-03-06 15:15:59 -0300

    Comments added

--- src/java/br/ufmg/engsoft/reprova/model/generator/IQuestionnaireGenerator.java
+++ src/java/br/ufmg/engsoft/reprova/model/generator/IQuestionnaireGenerator.java
@@ -3,6 +3,12 @@ package br.ufmg.engsoft.reprova.model.generator;
 import br.ufmg.engsoft.reprova.model.Questionnaire;
 import br.ufmg.engsoft.reprova.database.QuestionsDAO;
 
+/*
+* The type IQuestionnaireGenerator
+*/
 public interface IQuestionnaireGenerator {
+  /*
+  * Generate IQuestionnaire
+  */
   public Questionnaire generate(QuestionsDAO questionsDAO, String averageDifficulty, int questionsCount, int totalEstimatedTime);
 }
