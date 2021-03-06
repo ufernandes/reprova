02176fcd2f38c0f51becbd968403090800a1b126
LorenaMendes <35077553+LorenaMendes@users.noreply.github.com>
2021-03-06 15:35:32 -0300

    Comments added

--- src/java/br/ufmg/engsoft/reprova/model/generator/ChainQuestionnaireGeneration.java
+++ src/java/br/ufmg/engsoft/reprova/model/generator/ChainQuestionnaireGeneration.java
@@ -3,18 +3,22 @@ package br.ufmg.engsoft.reprova.model.generator;
 import br.ufmg.engsoft.reprova.model.Questionnaire;
 import java.lang.UnsupportedOperationException;
 
+/* The type ChainQuestionnaireGeneration */
 abstract class ChainQuestionnaireGeneration {
   ChainQuestionnaireGeneration next;
   IQuestionnaireGenerator generator;
 
+  /* Set next */
   public void setNext(ChainQuestionnaireGeneration next) {
     this.next = next;
   }
 
+   /* Generate */
   Questionnaire generate(Questionnaire questionnaire) {
     throw new UnsupportedOperationException("Method not implemented on abstract level.");
   }
   
+  /* Handle Generation */
   Questionnaire handleGeneration(Questionnaire questionnaire) {
     if (next != null) {
       return next.generate(questionnaire);
